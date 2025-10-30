980c4b51a4878d31338ea8ee40e7ad48e88e88ce
Louise Bruwer <u23673941@tuks.co.za>
2025-10-30 20:18:01 +0200

    COMMIT before merge

--- frontend/src/components/FilesList.js
+++ frontend/src/components/FilesList.js
@@ -1,17 +1,16 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
 const FilesList = ({ files }) => (
   <section>
     <h2>Files</h2>
-    {files.length === 0 ? (
+    {!files || files.length === 0 ? (
       <p>No files yet.</p>
     ) : (
       <ul>
-        {files.map((file) => (
-          <li key={file.fileUrl}>
-            <a href={`/uploads/projects/${file.fileUrl}`} download>
-              {file.filename}
+        {files.map((file, i) => (
+          <li key={i}>
+            <a href={`/uploads/projects/${file}`} download>
+              {file}
             </a>
           </li>
         ))}
@@ -21,3 +20,29 @@ const FilesList = ({ files }) => (
 );
 
 export default FilesList;
+
+
+
+// import React from "react";
+// import { Link } from "react-router-dom";
+
+// const FilesList = ({ files }) => (
+//   <section>
+//     <h2>Files</h2>
+//     {files.length === 0 ? (
+//       <p>No files yet.</p>
+//     ) : (
+//       <ul>
+//         {files.map((file) => (
+//           <li key={file.fileUrl}>
+//             <a href={`/uploads/projects/${file.fileUrl}`} download>
+//               {file.filename}
+//             </a>
+//           </li>
+//         ))}
+//       </ul>
+//     )}
+//   </section>
+// );
+
+// export default FilesList;
